0369d48d6806db3bcc11979c53e769d3a4541312
Isaac Iniongun <iniongunisaac@gmail.com>
2020-07-03 15:44:10 +0100

    feat: Setup
    - Initial Project creation with modules.

---
+++ common/src/main/java/com/iniongungroup/mobile/droid/atsamaikyenge/common/base/BaseFragment.kt
@@ -0,0 +1,9 @@
+package com.iniongungroup.mobile.droid.atsamaikyenge.common.base
+
+/**
+ * Created by Isaac Iniongun on 03/07/2020.
+ * For Atsam a Ikyenge project.
+ */
+
+class BaseFragment {
+}
---
+++ common/src/main/java/com/iniongungroup/mobile/droid/atsamaikyenge/common/utils/Utils+Extensions.kt
@@ -0,0 +1,7 @@
+package com.iniongungroup.mobile.droid.atsamaikyenge.common.utils
+
+/**
+ * Created by Isaac Iniongun on 03/07/2020.
+ * For Atsam a Ikyenge project.
+ */
+
