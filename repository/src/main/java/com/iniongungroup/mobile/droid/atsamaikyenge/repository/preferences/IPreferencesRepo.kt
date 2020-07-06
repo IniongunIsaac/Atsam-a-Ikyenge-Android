cf8fc53dad20362c462b721624634167bfb67068
Isaac Iniongun <iniongunisaac@gmail.com>
2020-07-06 11:17:02 +0100

    feat: Repo-Preferences
    - Add app repo interfaces and implement them in preferences module.

---
+++ repository/src/main/java/com/iniongungroup/mobile/droid/atsamaikyenge/repository/preferences/IPreferencesRepo.kt
@@ -0,0 +1,14 @@
+package com.iniongungroup.mobile.droid.atsamaikyenge.repository.preferences
+
+/**
+ * Created by Isaac Iniongun on 06/07/2020.
+ * For Atsam a Ikyenge project.
+ */
+
+interface IPreferencesRepo {
+
+    var isDBInitialized: Boolean
+
+    var currentTheme: String
+
+}
